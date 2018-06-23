06e9eb976e944e6781d0ca0e92f188b5875c12fc
yoshi <yoshiharuseijin@gmail.com>
2018-06-23 19:16:51 +0900

    [feat]: 自分のアカウント以外を見てフォローするように変更

--- twitter.rb
+++ twitter.rb
@@ -18,10 +18,18 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ACCESS_TOKEN_SECRET
 end
 
-limit = 10
-
 tag = "エンジニア生存戦略"
 
-client.search("##{tag}", lang: "ja", result_type: "recent", count: 1).take(limit).map do |tweet|
-  puts "#{tweet.user.name}: #{tweet.text}"
+# count: 1ページに返すtweetの数。最大100件まで。
+# take: 取得件数。なので最大100件取得が10回回る？
+# iのインクリメント検証では678回回った。
+ i = 0
+client.search("##{tag}", lang: "ja", result_type: "recent", count: 100).take(1000).map do |tweet|
+   i += 1
+   puts "#{i}件目のツイート"
+  if tweet.user.id != MY_TWITTER_ID then
+    client.follow(tweet.user.id)
+    puts "#Followed #{tweet.user.id}"
+  end
+
 end
